b6156a203da6159d383e36496aeef5c9f5e9c9f1
Personal <Personal@DESKTOP-GR0841O>
2023-07-16 12:21:16 +0530

    Handling a case where time is negative

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function() {
-  loader.style.display = "none";
+window.addEventListener("load", function () {
+    loader.style.display = "none";
 });
 
-let launchDate = new Date("October 5, 2023 00:00:00").getTime();
+let launchDate = new Date("July 16, 2023 00:00:00").getTime();
 
 let timer = setInterval(tick, 1000);
 
 function tick() {
     let now = new Date().getTime();
     let t = launchDate - now;
-    if(t>0){
-        let days = Math.floor(t / (1000 * 60 * 60 *24));
-        if(days<10)
-        {
+    if (t > 0) {
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        if (days < 10) {
             days = "0" + days;
         }
-        let hours = Math.floor(t % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ));
-        if(hours<10)
-        {
+        let hours = Math.floor(t % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+        if (hours < 10) {
             hours = "0" + hours;
         }
         let minutes = Math.floor(t % (1000 * 60 * 60) / (1000 * 60));
-        if(minutes<10)
-        {
+        if (minutes < 10) {
             minutes = "0" + minutes;
         }
         let seconds = Math.floor(t % (1000 * 60) / 1000);
-        if(seconds<10)
-        {
+        if (seconds < 10) {
             seconds = "0" + seconds;
         }
 
@@ -36,11 +32,17 @@ function tick() {
 
         document.querySelector('.countdown').innerText = time;
     }
+    else {
+        let time = `The Fest is Live Now!`;
+
+        document.querySelector('.countdown').innerText = time;
+    }
+
 }
 
 const menuToggle = document.querySelector('.menu-toggle input');
 const menu = document.querySelector('.menu');
 
-menuToggle.addEventListener('click', function() {
-  menu.classList.toggle('slide');
+menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('slide');
 });

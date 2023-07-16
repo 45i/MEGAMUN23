d593d0245472d799bce70a48e29d85473296cc7c
yessboialt <88524941+yessboialt@users.noreply.github.com>
2023-07-16 18:08:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+    .split("")
+    .map((letter, index) =>{
+        if(index<iterations){
+            return event.target.dataset.value[index];
+        }    
+    return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+    if(iterations>= event.target.dataset.value.length) {
+        clearInterval(interval); 
+    }
+
+    iterations += 1/2;
+
+ }, 70);
+};
+
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function () {
-    loader.style.display = "none";
+window.addEventListener("load", function() {
+  loader.style.display = "none";
 });
 
 let launchDate = new Date("October 5, 2023 00:00:00").getTime();
@@ -10,39 +35,37 @@ let timer = setInterval(tick, 1000);
 function tick() {
     let now = new Date().getTime();
     let t = launchDate - now;
-    if (t > 0) {
-        let days = Math.floor(t / (1000 * 60 * 60 * 24));
-        if (days < 10) {
+    if(t>0){
+        let days = Math.floor(t / (1000 * 60 * 60 *24));
+        if(days<10)
+        {
             days = "0" + days;
         }
-        let hours = Math.floor(t % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-        if (hours < 10) {
+        let hours = Math.floor(t % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ));
+        if(hours<10)
+        {
             hours = "0" + hours;
         }
         let minutes = Math.floor(t % (1000 * 60 * 60) / (1000 * 60));
-        if (minutes < 10) {
+        if(minutes<10)
+        {
             minutes = "0" + minutes;
         }
         let seconds = Math.floor(t % (1000 * 60) / 1000);
-        if (seconds < 10) {
+        if(seconds<10)
+        {
             seconds = "0" + seconds;
         }
 
-        let time = `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
+        let time = `${days} : ${hours} : ${minutes} : ${seconds}`;
 
         document.querySelector('.countdown').innerText = time;
     }
-    else {
-        let time = `The Fest is Live Now!`;
-
-        document.querySelector('.countdown').innerText = time;
-    }
-
 }
 
 const menuToggle = document.querySelector('.menu-toggle input');
 const menu = document.querySelector('.menu');
 
-menuToggle.addEventListener('click', function () {
-    menu.classList.toggle('slide');
+menuToggle.addEventListener('click', function() {
+  menu.classList.toggle('slide');
 });

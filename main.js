e142366ea6bdf0626ab16e92634a4681954e153f
Personal <Personal@DESKTOP-GR0841O>
2023-07-17 16:48:58 +0530

    hh

--- main.js
+++ main.js
@@ -24,7 +24,25 @@ document.querySelectorAll("[data-value]").forEach(element => {
   });
 
   element.addEventListener('mouseleave', event => {
-    event.target.innerText = event.target.dataset.value;
+      let iterations = 0;
+
+      const interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+              .split("")
+              .map((letter, index) => {
+                  if (index < iterations) {
+                      return event.target.dataset.value[ index ];
+                  }
+                  return letters[ Math.floor(Math.random() * (26 + 10)) ];
+              })
+              .join("");
+
+          if (iterations >= event.target.dataset.value.length) {
+              clearInterval(interval);
+          }
+
+          iterations += 1 / 2;
+      }, 100);                  
   });
 });
 var loader = document.getElementById("preloader");

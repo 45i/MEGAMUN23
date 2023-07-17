63c3f9ecf3f17ac8849428ed65e22971820bdc73
Personal <Personal@DESKTOP-GR0841O>
2023-07-17 19:26:34 +0530

    try?

--- main.js
+++ main.js
@@ -45,6 +45,51 @@ document.querySelectorAll("[data-value]").forEach(element => {
       }, 100);                  
   });
 });
+// Define a function to insert the navbar
+function insertNavbar() {
+    // Fetch the contents of the navbar HTML file
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            // Insert the navbar HTML into the navbar-container element
+            const navbarContainers = document.querySelectorAll('.navbar-container');
+            navbarContainers.forEach(navbarContainer => {
+                navbarContainer.innerHTML = data;
+
+                // Attach event listeners to the navbar elements
+                navbarContainer.querySelectorAll("[data-value]").forEach(element => {
+                    element.addEventListener('mouseenter', event => {
+                        let iterations = 0;
+
+                        const interval = setInterval(() => {
+                            event.target.innerText = event.target.innerText
+                                .split("")
+                                .map((letter, index) => {
+                                    if (index < iterations) {
+                                        return event.target.dataset.value[ index ];
+                                    }
+                                    return letters[ Math.floor(Math.random() * (26 + 10)) ];
+                                })
+                                .join("");
+
+                            if (iterations >= event.target.dataset.value.length) {
+                                clearInterval(interval);
+                            }
+
+                            iterations += 1 / 2;
+                        }, 100);
+                    });
+
+                    element.addEventListener('mouseleave', event => {
+                        event.target.innerText = event.target.dataset.value;
+                    });
+                });
+            });
+        });
+}
+
+// Call the insertNavbar function when the DOM is ready
+document.addEventListener('DOMContentLoaded', insertNavbar);
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
     loader.style.display = "none";

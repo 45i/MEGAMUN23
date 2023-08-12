e00e02c88d86a7312ffe0afecd32ee22c5630985
Personal <Personal@DESKTOP-GR0841O>
2023-08-12 15:36:35 +0530

    hg

--- main2.js
+++ main2.js
@@ -12,35 +12,35 @@ const menu = document.querySelector('.menu');
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('slide');
 });
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
-document.querySelectorAll("[data-value]").forEach(element => {
-    element.addEventListener('mouseenter', event => {
-        let iterations = 0;
+// document.querySelectorAll("[data-value]").forEach(element => {
+//     element.addEventListener('mouseenter', event => {
+//         let iterations = 0;
 
-        const interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-                .split("")
-                .map((letter, index) => {
-                    if (index < iterations) {
-                        return event.target.dataset.value[ index ];
-                    }
-                    return letters[ Math.floor(Math.random() * (26 + 10)) ];
-                })
-                .join("");
+//         const interval = setInterval(() => {
+//             event.target.innerText = event.target.innerText
+//                 .split("")
+//                 .map((letter, index) => {
+//                     if (index < iterations) {
+//                         return event.target.dataset.value[ index ];
+//                     }
+//                     return letters[ Math.floor(Math.random() * (26 + 10)) ];
+//                 })
+//                 .join("");
 
-            if (iterations >= event.target.dataset.value.length) {
-                clearInterval(interval);
-            }
+//             if (iterations >= event.target.dataset.value.length) {
+//                 clearInterval(interval);
+//             }
 
-            iterations += 1 / 2;
-        }, 100);
-    });
+//             iterations += 1 / 2;
+//         }, 100);
+//     });
 
-    element.addEventListener('mouseleave', event => {
-        event.target.innerText = event.target.dataset.value;
-    });
-});
+//     element.addEventListener('mouseleave', event => {
+//         event.target.innerText = event.target.dataset.value;
+//     });
+// });
 
 var googleMap = document.querySelector('.google-map');
 var mapHeight = 0;

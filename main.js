8b866fe617cc2903845349187891a20f48c9020c
Personal <Personal@DESKTOP-GR0841O>
2023-09-30 15:29:14 +0530

    k

--- main.js
+++ main.js
@@ -8,19 +8,6 @@
 //   });
 // });
 
-var swiper = new Swiper('.blog-slider', {
-  spaceBetween: 30,
-  effect: 'fade',
-  loop: true,
-  mousewheel: {
-    invert: false,
-  },
-  // autoHeight: true,
-  pagination: {
-    el: '.blog-slider__pagination',
-    clickable: true,
-  }
-});
 
 const body = document.body;
 

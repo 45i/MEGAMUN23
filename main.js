651865c846a914bba005427a96016e2039eee088
Personal <Personal@DESKTOP-GR0841O>
2023-09-30 09:49:22 +0530

    d

--- main.js
+++ main.js
@@ -8,6 +8,41 @@
 //   });
 // });
 
+const body = document.body;
+
+let sx = 0, // For scroll positions
+    sy = 0;
+let dx = sx, // For container positions
+    dy = sy;
+
+// Bind a scroll function
+window.addEventListener('scroll', easeScroll);
+
+function easeScroll() {
+  sx = window.pageXOffset;
+  sy = window.pageYOffset;
+}
+
+window.requestAnimationFrame(render);
+
+function render() {
+  // We calculate our container position by linear interpolation method
+  dx = li(dx, sx, 0.2);
+  dy = li(dy, sy, 0.2);
+
+  dx = Math.floor(dx * 100) / 100;
+  dy = Math.floor(dy * 100) / 100;
+
+  body.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+  window.requestAnimationFrame(render);
+}
+
+function li(a, b, n) {
+  return (1 - n) * a + n * b;
+}
+
+
 function supportsBackdropFilter() {
   const testElement = document.createElement('div');
   testElement.style.backdropFilter = 'blur(10px)';
